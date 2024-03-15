d6f15a16379ad16d67151e98573277317f5a3570
大智 江原 <d.ehara2019@gmail.com>
2024-03-15 17:57:27 +0900

    Add SellsController

---
+++ Manga.Server/Controllers/SellsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Manga.Server.Data;
+using Manga.Server.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Manga.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<UserAccount> _userManager;
+
+
+        public SellsController(ApplicationDbContext context, UserManager<UserAccount> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Sells
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sell>>> GetSell()
+        {
+            return await _context.Sell.ToListAsync();
+        }
+
+        // GET: api/Sells/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sell>> GetSell(int id)
+        {
+            var sell = await _context.Sell.FindAsync(id);
+
+            if (sell == null)
+            {
+                return NotFound();
+            }
+
+            return sell;
+        }
+
+        [HttpGet("HomeData")]
+        public async Task<ActionResult<List<HomeDto>>> GetHomeDataAsync()
+        {
+            // 仮のユーザーID - 実際には認証情報から取得する必要があります
+            var currentUserId = _userManager.GetUserId(User);
+            if (currentUserId == null)
+            {
+                return NotFound();
+            }
+
+            // 現在のユーザーのOwnedListのタイトルリストを取得
+            var ownedTitles = await _context.OwnedList
+                                             .Where(o => o.UserAccountId == currentUserId)
+                                             .Select(o => o.Title)
+                                             .ToListAsync();
+
+            var sells = await _context.Sell.Include(s => s.SellImages).ToListAsync();
+            var homeDtos = new List<HomeDto>();
+
+            foreach (var sell in sells)
+            {
+                var wishTitles = await _context.WishList
+                                               .Where(w => w.UserAccountId == sell.UserAccountId)
+                                               .Select(w => new WishTitleInfo
+                                               {
+                                                   Title = w.Title,
+                                                   // タイトルが現在のユーザーのOwnedListに含まれる場合、IsOwnedをtrueに設定
+                                                   IsOwned = ownedTitles.Contains(w.Title)
+                                               })
+                                               .ToListAsync();
+
+                var dto = new HomeDto
+                {
+                    SellTitle = sell.Title,
+                    WishTitles = wishTitles,
+                    SellImage = sell.SellImages
+                                    .OrderBy(si => si.Order)
+                                    .FirstOrDefault()?.ImageUrl
+                };
+
+                homeDtos.Add(dto);
+            }
+
+            return homeDtos;
+        }
+
+
+
+
+        // PUT: api/Sells/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSell(int id, Sell sell)
+        {
+            if (id != sell.SellId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sell).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SellExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sells
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sell>> PostSell(Sell sell)
+        {
+            _context.Sell.Add(sell);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSell", new { id = sell.SellId }, sell);
+        }
+
+        // DELETE: api/Sells/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSell(int id)
+        {
+            var sell = await _context.Sell.FindAsync(id);
+            if (sell == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sell.Remove(sell);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SellExists(int id)
+        {
+            return _context.Sell.Any(e => e.SellId == id);
+        }
+    }
+}
--- Manga.Server/Models/SellImage.cs
+++ Manga.Server/Models/SellImage.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string ImageUrl { get; set; }
+        public int Order { get; set; }
 
         public int SellId { get; set; }
         public virtual Sell Sell { get; set; }
