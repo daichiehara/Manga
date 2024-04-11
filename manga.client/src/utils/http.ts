2a32700cec2225669e8a32bcacb143e2defdb41f
katakori99 <nameki.seito@gmail.com>
2024-04-11 18:42:03 +0900

    http.tsエラーを分かりやすくした。

--- manga.client/src/utils/http.ts
+++ manga.client/src/utils/http.ts
@@ -5,35 +5,34 @@ import { updateGlobalAuthState } from '../components/auth/AuthContext';
 console.log('http.tsxが読み込まれた。');
 axios.interceptors.response.use(
     response => {
-      console.log('H1_Responseを受け取った。:', response);
+      console.log('H1_Responseを受け取った。RefreshTokenは叩かれない。:', response);
       return response;
     },
     async error => {
       console.error('H2_何かしらErrorを受け取った。:', error.response);
       // 400 Bad Requestの場合、ログインページにリダイレクト
-      if (error.response.status === 400) {
-        console.log('H_3 本来はログインページ');
-      }
-      if (error.response.status === 401) {
-        try {
-          await authService.refreshToken();
-          console.log('H_4Token refreshed successfully');
-          
-          history.go(0);
-          //本来はlogin関数とかで認証状態に？
-        } catch (refreshError) {
-          console.error('H_5リフレッシュトークンに弾かれた。本来はログインページへ。:', refreshError);
-          updateGlobalAuthState({ isAuthenticated: false }); // 認証状態をリセット
-          // ログインページへ？
-          return Promise.reject(refreshError);
+      if (error.response) {
+        // 特定のエラー応答に基づく処理
+        if (error.response.status === 400) {
+          console.log('H_3 400 Bad Requestのエラー');
+        } else if (error.response.status === 401) {
+          try {
+            await authService.refreshToken();
+            console.log('H_4 Token refreshed successfully');
+            history.go(0);
+          } catch (refreshError) {
+            console.error('H_5 リフレッシュトークンにハジかれた。:', refreshError);
+            updateGlobalAuthState({ isAuthenticated: false });
+            return Promise.reject(refreshError);
+          }
+        } else {
+          console.error('H_その他のエラー。ステータスコード:', error.response.status);
         }
-      
-      }
-      else {
-        console.error('H_例外');
+      } else {
+        console.error('H_レスポンス無しのエラー:', error);
       }
       return Promise.reject(error);
     }
-);
+  );
   
   // ... 他のHTTP設定
