2592f4a4fa876629671ffec8075dccd66b8b40f2
katakori99 <nameki.seito@gmail.com>
2024-04-11 14:14:36 +0900

    ログイン状態を維持する機能。認証を保持する。

--- manga.client/src/utils/http.ts
+++ manga.client/src/utils/http.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { authService } from '../api/authService';
+import { updateGlobalAuthState } from '../components/auth/AuthContext';
 
 console.log('http.tsxが読み込まれた。');
-
 axios.interceptors.response.use(
     response => {
       console.log('H1_Responseを受け取った。:', response);
@@ -18,14 +18,19 @@ axios.interceptors.response.use(
         try {
           await authService.refreshToken();
           console.log('H_4Token refreshed successfully');
+          
           history.go(0);
           //本来はlogin関数とかで認証状態に？
         } catch (refreshError) {
           console.error('H_5リフレッシュトークンに弾かれた。本来はログインページへ。:', refreshError);
+          updateGlobalAuthState({ isAuthenticated: false }); // 認証状態をリセット
           // ログインページへ？
           return Promise.reject(refreshError);
         }
       
+      }
+      else {
+        console.error('H_例外');
       }
       return Promise.reject(error);
     }
