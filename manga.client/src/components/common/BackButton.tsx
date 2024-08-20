7d6a4191924b5419ab0ee211f019d23f8bdec2b3
katakori99 <nameki.seito@gmail.com>
2024-08-20 18:25:22 +0900

    BackButtunとその他，細かい調整

--- manga.client/src/components/common/BackButton.tsx
+++ manga.client/src/components/common/BackButton.tsx
@@ -1,30 +1,49 @@
 import React from 'react';
-import { IconButton, IconButtonProps } from '@mui/material';
+import { Box, IconButton, IconButtonProps } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
 interface BackButtonProps extends IconButtonProps {
   handleBack: () => void;
 }
 
 const BackButton: React.FC<BackButtonProps> = ({ handleBack, ...rest }) => (
-  <IconButton
-    onClick={handleBack}
-    {...rest}
+  <Box
     sx={{
+      // バックボタンを画面に固定するための設定
       position: 'fixed',
-      zIndex: 10000,
-      top: 8,  // Fixed button position to top-left corner with some padding
-      left: 8, // Fixed button position to top-left corner with some padding
+      zIndex: 10000,  // 他の要素より上に表示されるようにするための設定
+      top: 8,
+      left: 8,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: 'rgba(255, 255, 255, 0.5)',
-      '&:hover': {
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+
+      // メディアクエリで画面サイズに応じて位置を調整
+      '@media (max-width: 640px)': {
+        // スマホサイズの場合、左上から8pxの位置に固定
+        left: '8px',
+        top: '8px',
+      },
+      '@media (min-width: 641px)': {
+        // パソコンサイズの場合、640px幅の中央に合わせて配置
+        left: 'calc(50% - 320px + 8px)',
+        top: '8px',
       },
     }}
   >
-    <ArrowBackIosNewIcon />
-  </IconButton>
+    <IconButton
+      onClick={handleBack}
+      {...rest}
+      sx={{
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',  // ボタンの背景色
+        '&:hover': {
+          backgroundColor: 'rgba(255, 255, 255, 0.7)',  // ホバー時の背景色
+        },
+      }}
+    >
+      <ArrowBackIosNewIcon />
+    </IconButton>
+  </Box>
 );
 
 export default BackButton;
