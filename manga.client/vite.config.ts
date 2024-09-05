d18fa2862052b53cc04a4649b51d0e537e5755ec
大智 江原 <d.ehara2019@gmail.com>
2024-09-05 16:29:20 +0900

    Add manifest

--- manga.client/vite.config.ts
+++ manga.client/vite.config.ts
@@ -6,6 +6,7 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
@@ -35,7 +36,41 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [
+        plugin(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+            manifest: {
+                "short_name": "トカエル",
+                "name": "Tocaeru-トカエル",
+                "icons": [
+                {
+                    "src": "TocaeruCircle.png",
+                    "sizes": "398x398",
+                    "type": "image/png"
+                },
+                {
+                    "src": "Tocaeru512Circle.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                },
+                ],
+                "start_url": "/",
+                "display": "standalone",
+                "theme_color": "#E97032",
+                "background_color": "#ffffff",
+                "description": "Tocaeru(トカエル)は日本初の漫画の物々交換を簡単に楽しめるサービスです。/n/nアプリをインストールしてより便利に！",
+                "screenshots": [
+                {
+                    "src": "https://manga-img-bucket.s3.ap-northeast-1.amazonaws.com/TocaeruLogo.webp",
+                    "sizes": "959x252",
+                    "type": "image/webp"
+                }
+                ]
+            }
+          })
+        ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
